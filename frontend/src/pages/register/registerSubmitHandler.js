ce13f68fd5df497c7c28fd9bf9f4bcf8e2da8dd3
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-03-28 20:05:11 +0200

    added the register function

---
+++ frontend/src/pages/register/registerSubmitHandler.js
@@ -0,0 +1,19 @@
+export async function registerSubmitHandler(username, password) {
+  try {
+    const data = await fetch(`http://localhost:5050/auth/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+
+    const responseText = await data.text();
+
+    return responseText;
+  } catch (error) {
+    const res = error.response.data;
+    return res;
+  }
+}
