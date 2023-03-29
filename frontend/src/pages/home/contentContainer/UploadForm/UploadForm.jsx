4ab7234a33a6936d970986e3f8b9c599727fd1e3
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-03-29 16:31:57 +0200

    added form component with style

---
+++ frontend/src/pages/home/contentContainer/UploadForm/UploadForm.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./uploadForm.css";
+
+function UploadForm({ handleFileSelect, handleSubmit, handleDescriptionText }) {
+  return (
+    <div id="uploadContainer">
+      <textarea
+        placeholder="description"
+        type="text"
+        onChange={handleDescriptionText}
+      />{" "}
+      <div className="buttonContainer">
+        {" "}
+        <div className="file-input">
+          {" "}
+          <label htmlFor="file-upload">Select Img</label>{" "}
+          <input id="file-upload" type="file" onChange={handleFileSelect} />
+        </div>
+        <button onClick={handleSubmit}>Upload</button>
+      </div>
+    </div>
+  );
+}
+
+export default UploadForm;
