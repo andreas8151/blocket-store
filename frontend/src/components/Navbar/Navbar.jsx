a5045b830d1f7de2c71fc4e1270d103790ece8d0
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-03-31 16:19:26 +0200

    added a navbar with style

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./navbar.css";
+
+function Navbar() {
+  return (
+    <div id="navContainer">
+      <div id="navLinks">
+        <a href="/">Home</a>
+        <a href="/">Products</a>
+      </div>
+      <div>
+        <a href="/">Logout</a>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
