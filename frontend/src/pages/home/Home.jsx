dfc7cac5c0c799254ea90ef997e302d682de741e
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-04-01 22:05:38 +0200

    added navbar component

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,16 @@
 import "./home.css";
 import AddFriendContainer from "./friends/FriendContainer";
 import UploadContentContainer from "./contentContainer/UploadContentContainer";
+import Navbar from "../../components/Navbar/Navbar";
 
 function Home() {
   return (
     <div className="homeContainer">
       {" "}
+      <div id="header">
+        <Navbar />
+        <div className="logoText"></div>
+      </div>
       <UploadContentContainer />
       <AddFriendContainer />
     </div>
