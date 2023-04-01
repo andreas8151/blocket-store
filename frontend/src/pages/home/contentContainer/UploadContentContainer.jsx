13209c13988f60f77100aeb89712cf3c9df27ab2
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-04-01 22:04:07 +0200

    update

--- frontend/src/pages/home/contentContainer/UploadContentContainer.jsx
+++ frontend/src/pages/home/contentContainer/UploadContentContainer.jsx
@@ -3,7 +3,9 @@ import ContentBox from "./ContentBox/ContentBox";
 import { fetchContent } from "./fetchContent";
 import { uploadContent } from "./uploadContent";
 import UploadForm from "./UploadForm/UploadForm";
+
 import "./uploadContentContainer.css";
+import MyContentBox from "./ContentBox/MyContentBox";
 
 function UploadImgContainer() {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -37,19 +39,23 @@ function UploadImgContainer() {
   }, []);
 
   return (
-    <div>
+    <div id="mainContainer">
+      {" "}
       <div>
+        {" "}
         <UploadForm
           handleFileSelect={handleFileSelect}
           handleSubmit={handleSubmit}
           handleDescriptionText={handleDescriptionText}
         />
       </div>
-
-      <div className="contentContainer">
-        {content.map((userContent, index) => (
-          <ContentBox userContent={userContent} key={index} index={index} />
-        ))}
+      <div className="myContent">
+        <h2>Your products</h2>
+        <div className="contentContainer">
+          {content.map((userContent, index) => (
+            <MyContentBox userContent={userContent} key={index} index={index} />
+          ))}
+        </div>
       </div>
     </div>
   );
